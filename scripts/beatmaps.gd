dfe045cb6004b83ea3e09f5ee0f000e0b0115396
Caio <77120914+cesadev@users.noreply.github.com>
2026-08-14 13:15:07 -0300

    Update beatmaps.gd

--- scripts/beatmaps.gd
+++ scripts/beatmaps.gd
@@ -18,3 +18,35 @@ var music_1 = {
 		2: [5,6,7,8],
 	}
 }
+
+var music_2 = {
+	"music":preload("res://assets/musics/skulls_adventure.mp3"),
+	"guitar":{
+		0.0: [1,2],
+		0.1: [1,2],
+		0.2: [1,2],
+		0.3: [1,2],
+		0.4: [1,2],
+		0.5: [1,2],
+	},
+	"drums": {
+		1: [1,2,3,4],
+		2: [5,6,7,8],
+	}
+}
+
+var music_3 = {
+	"music":preload("res://assets/musics/skulls_adventure.mp3"),
+	"guitar":{
+		0.0: [1,2],
+		0.1: [1,2],
+		0.2: [1,2],
+		0.3: [1,2],
+		0.4: [1,2],
+		0.5: [1,2],
+	},
+	"drums": {
+		1: [1,2,3,4],
+		2: [5,6,7,8],
+	}
+}
